3371f13e3df32a6d41bd1e2449b4c8538afd8236
Cheng-Hung Tsai <ctsai085@ucr.edu>
2021-01-23 22:58:55 -0800

    Saving output to mysql

--- run_iupred_genome-wide.py
+++ run_iupred_genome-wide.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
-from Bio import SeqIO
-import iupred2a_lib
+import gzip
 import pickle
+
+import iupred2a_lib
 import numpy as np
+from Bio import SeqIO
+
+import numpy as np
+import pandas as pd
+from util.py import connect_to_db
 
 def idr_detector(arr, thresh=0.5, window_size=30):
     m = arr >= thresh
@@ -12,25 +18,24 @@ def idr_detector(arr, thresh=0.5, window_size=30):
     lens = regions[:, 1] - regions[:, 0]
     return regions[(lens >= window_size)]
 
-inputfile = "orf_trans.fasta"
-IDP = []
-IDR_percentage = []
-IDR = []
+species = 'Neurospora_crassa'
+fasta = 'pep/Neurospora_crassa.NC12.pep.all.fa.gz'
 
-for record in SeqIO.parse(inputfile, "fasta"):
-    iupred_scores = np.asarray(iupred2a_lib.iupred(record.seq, "long"))
-    IDR_in_entry = idr_detector(iupred_scores)
-    if IDR_in_entry.size != 0:
-        IDP.append(record.id)
-        IDR_lens = IDR_in_entry[:, 1] - IDR_in_entry[:, 0]
-        IDR_percentage.append(IDR_lens.sum() / len(record.seq))
-        IDR.append(IDR_in_entry)
+df = pd.DataFrame(columns=['IDR_start', 'IDR_end', 'IDP'])
+with gzip.open(fasta, 'rt') as fh:
+    for record in SeqIO.parse(fh, "fasta"):
+        iupred_scores = np.asarray(iupred2a_lib.iupred(record.seq, "long"))
+        IDR_in_entry = idr_detector(iupred_scores)
+        if IDR_in_entry.size != 0:
+            IDR_lens = IDR_in_entry[:, 1] - IDR_in_entry[:, 0]
+            # IDR_percentage.append(IDR_lens.sum() / len(record.seq))
+            tmp_df = pd.DataFrame(IDR_in_entry, columns=['IDR_start', 'IDR_end'])
+            tmp_df['IDP'] = record.id
+            df = df.append(tmp_df, ignore_index=True)
 
-# Save output to pickle file
-with open('IDP_S288c.pickle', 'wb') as fh:
-    pickle.dump((IDP, IDR_percentage, IDR), fh)
+# Reorder the dataframe
+df = df[['IDP', 'IDR_start', 'IDR_end']]
 
-parser = list(SeqIO.parse(inputfile, "fasta"))
-lens_of_total_protein = len(parser)
-print("Disordered protein : {} out of {}".format(len(IDR_percentage, lens_of_total_protein)))
-print("Average disordered regions length of IDP: {}".format(np.average(np.asarray(IDR_percentage))))
+# Save to database
+engine = connect_to_db()
+df.to_sql(species, con=engine, if_exists='replace', index_label='ID')
