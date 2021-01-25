6c405e6f6b2159ff01d9ce2a83c6e202f132c526
Cheng-Hung Tsai <ctsai085@ucr.edu>
2021-01-24 23:57:38 -0800

    Update code to save all IDR in a single column

--- run_iupred_genome-wide.py
+++ run_iupred_genome-wide.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
 import gzip
-import os
-import pickle
-import sys
 
 import iupred2a_lib
 import numpy as np
 import pandas as pd
 from Bio import SeqIO
-
-# For slurm to access functions in util
-sys.path.append(os.getcwd())
-from util import connect_to_db
 
 
 def idr_detector(arr, thresh=0.5, window_size=30):
@@ -22,24 +15,30 @@ def idr_detector(arr, thresh=0.5, window_size=30):
     lens = regions[:, 1] - regions[:, 0]
     return regions[(lens >= window_size)]
 
-species = 'Neurospora_crassa'
-fasta = 'pep/Neurospora_crassa.NC12.pep.all.fa.gz'
+# Metadata, would be constructed with dataframe format in the future
+species_lis = ['Neurospora_crassa', 'Aspergillus_fumigatus']
+fasta_lis = ['pep/Neurospora_crassa.NC12.pep.all.fa.gz', 'pep/Aspergillus_fumigatus.ASM265v1.pep.all.fa.gz']
 
-df = pd.DataFrame(columns=['IDR_start', 'IDR_end', 'IDP'])
-with gzip.open(fasta, 'rt') as fh:
-    for record in SeqIO.parse(fh, "fasta"):
-        iupred_scores = np.asarray(iupred2a_lib.iupred(record.seq, "long"))
-        IDR_in_entry = idr_detector(iupred_scores)
-        if IDR_in_entry.size != 0:
-            IDR_lens = IDR_in_entry[:, 1] - IDR_in_entry[:, 0]
-            # IDR_percentage.append(IDR_lens.sum() / len(record.seq))
-            tmp_df = pd.DataFrame(IDR_in_entry, columns=['IDR_start', 'IDR_end'])
-            tmp_df['IDP'] = record.id
-            df = df.append(tmp_df, ignore_index=True)
+df = pd.DataFrame(columns=['IDP', 'IDR_percentage', 'IDR', 'Species'])
+for species, fasta in zip(species_lis, fasta_lis):
+    sub_df = pd.DataFrame(columns=['IDP', 'IDR_percentage', 'IDR'])
+    with gzip.open(fasta, 'rt') as fh:
+        for record in SeqIO.parse(fh, "fasta"):
+            iupred_scores = np.asarray(iupred2a_lib.iupred(record.seq, "long"))
+            IDR_in_entry = idr_detector(iupred_scores)
+            if IDR_in_entry.size != 0:
+                IDR_lens = IDR_in_entry[:, 1] - IDR_in_entry[:, 0]
+                IDR_percentage = IDR_lens.sum() / len(record.seq)
+                lis = IDR_in_entry.tolist()
+                string = ';'.join(str(e) for e in lis)
+                string = string.replace(',', ':')
+                tmp_df = pd.Series([record.id, IDR_percentage, string], index=['IDP', 'IDR_percentage', 'IDR'])
+                sub_df = pd.concat([sub_df, tmp_df], ignore_index=True)
+        sub_df['Species'] = species
+    df = pd.concat([df, sub_df], ignore_index=True)
 
 # Reorder the dataframe
-df = df[['IDP', 'IDR_start', 'IDR_end']]
+df = df[['Species', 'IDP', 'IDR_percentage', 'IDR']]
 
-# Save to database
-engine = connect_to_db()
-df.to_sql(species, con=engine, if_exists='replace', index_label='ID')
+# Save to csv file
+df.to_csv('result_IDR_in_column.csv', index=False)
