33e4229ca6742595ddf602c457c40ef0b64cdce7
Cheng-Hung Tsai <ctsai085@ucr.edu>
2021-01-29 09:52:23 -0800

    Update for processing all data by sbatch job array

---
+++ job_array.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -J iupred2a_in_fungi
+#SBATCH --time=7-00:00:00
+#SABTCH -n 8
+#SBATCH --mem=10g
+#SBATCH --array=0-1013%8
+#SBATCH --output=slurm_log/%x_%A_%a.out
+
+files=( pep/* )
+input=${files[$SLURM_ARRAY_TASK_ID]}
+sample=${input%.pep.all.fa.gz}
+sample=${sample#pep/}
+
+mkdir -p output
+output=output/$sample
+
+echo $SLURM_ARRAY_TASK_ID, $sample
+./run_iupred_genome-wide.py --i $input --o $output
--- run_iupred_genome-wide.py
+++ run_iupred_genome-wide.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import gzip
+import argparse
 
 import iupred2a_lib
 import numpy as np
@@ -15,29 +16,34 @@ def idr_detector(arr, thresh=0.5, window_size=30):
     lens = regions[:, 1] - regions[:, 0]
     return regions[(lens >= window_size)]
 
-# Metadata, would be constructed with dataframe format in the future
-species_lis = ['Neurospora_crassa', 'Aspergillus_fumigatus']
-fasta_lis = ['pep/Neurospora_crassa.NC12.pep.all.fa.gz', 'pep/Aspergillus_fumigatus.ASM265v1.pep.all.fa.gz']
 
-df = pd.DataFrame(columns=['IDR_start', 'IDR_end', 'IDR_percentage', 'IDP', 'Species'])
-for species, fasta in zip(species_lis, fasta_lis):
-    sub_df = pd.DataFrame(columns=['IDR_start', 'IDR_end', 'IDR_percentage', 'IDP'])
-    with gzip.open(fasta, 'rt') as fh:
-        for record in SeqIO.parse(fh, "fasta"):
-            iupred_scores = np.asarray(iupred2a_lib.iupred(record.seq, "long"))
-            IDR_in_entry = idr_detector(iupred_scores)
-            if IDR_in_entry.size != 0:
-                IDR_lens = IDR_in_entry[:, 1] - IDR_in_entry[:, 0]
-                IDR_percentage = IDR_lens / len(record.seq)
-                tmp_df = pd.DataFrame(IDR_in_entry, columns=['IDR_start', 'IDR_end'])
-                tmp_df['IDR_percentage'] = IDR_percentage
-                tmp_df['IDP'] = record.id
-                sub_df = pd.concat([sub_df, tmp_df], ignore_index=True)
-        sub_df['Species'] = species
-    df = pd.concat([df, sub_df], ignore_index=True)
+# Parser
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--i', metavar='<Input.gz>', help='Peptide fasta.gz')
+parser.add_argument('--o', metavar='<Output.csv>', help='File name of output csv')
+args = parser.parse_args()
+
+# Remove prefix and postfix in input path
+fasta = args.i
+species = fasta.strip('pep/').strip('.pep.all.fa.gz')
+
+
+df = pd.DataFrame(columns=['IDR_start', 'IDR_end', 'IDR_percentage', 'IDP'])
+with gzip.open(fasta, 'rt') as fh:
+    for record in SeqIO.parse(fh, "fasta"):
+        iupred_scores = np.asarray(iupred2a_lib.iupred(record.seq, "long"))
+        IDR_in_entry = idr_detector(iupred_scores)
+        if IDR_in_entry.size != 0:
+            IDR_lens = IDR_in_entry[:, 1] - IDR_in_entry[:, 0]
+            IDR_percentage = IDR_lens / len(record.seq)
+            tmp_df = pd.DataFrame(IDR_in_entry, columns=['IDR_start', 'IDR_end'])
+            tmp_df['IDR_percentage'] = IDR_percentage
+            tmp_df['IDP'] = record.id
+            df = pd.concat([df, tmp_df], ignore_index=True)
+df['Species'] = species
 
 # Reorder the dataframe
 df = df[['Species', 'IDP', 'IDR_start', 'IDR_end', 'IDR_percentage']]
 
 # Save to csv file
-df.to_csv('result_IDR_by_rows.csv', index=False)
+df.to_csv(f'{args.o}.csv', index=False)
