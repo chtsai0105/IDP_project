c6f9f231b0124f59a3882777743bbefabe2f6768
Cheng-Hung Tsai <ctsai085@ucr.edu>
2021-03-21 00:42:58 -0700

    Code for retrieving IDP/peptide percentage from all species

---
+++ get_IDP_percentage_by_species.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Species,Total_protein,Total_IDP" > stat/IDP_percentage.csv
+
+for species in pep/*
+do
+    total_prot=`zcat $species | grep \> | wc -l`
+    species=${species%.pep.all.fa.gz}
+    species=${species#pep/}
+    total_IDP=`expr $(cat output/${species}_IDP.csv | wc -l) - 1`
+    species=`echo $species | awk '{match($0, /^_?[^_]+_[^_.]+/); print substr($0, RSTART, RLENGTH)}'` # Remove suffix
+    echo $species, $total_prot, $total_IDP
+done
+# done >> stat/IDP_percentage.csv
