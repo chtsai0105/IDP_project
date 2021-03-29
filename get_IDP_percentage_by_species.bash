441936f866fe88b24186c445e6356e126df0ef98
Cheng-Hung Tsai <ctsai085@ucr.edu>
2021-03-29 12:25:39 -0700

    Remove codes for retrieving total protein numbers from fasta (Use metadata instead)

--- get_IDP_percentage_by_species.bash
+++ get_IDP_percentage_by_species.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo "Species,Total_protein,Total_IDP" > stat/IDP_percentage.csv
+echo "FILENAME,TOTAL_IDP,NAME" > ./lib/IDP_percentage.csv
 
-for species in pep/*
+for file in ./pep/*
 do
-    total_prot=`zcat $species | grep \> | wc -l`
-    species=${species%.pep.all.fa.gz}
-    species=${species#pep/}
-    total_IDP=`expr $(cat output/${species}_IDP.csv | wc -l) - 1`
-    species=`echo $species | awk '{match($0, /^_?[^_]+_[^_.]+/); print substr($0, RSTART, RLENGTH)}'` # Remove suffix
-    echo $species, $total_prot, $total_IDP
-done >> stat/IDP_percentage.csv
+    acc=`basename $file`
+    acc=${acc%.pep.all.fa.gz}
+    total_IDP=`expr $(cat output/${acc}_IDP.csv | wc -l) - 1`
+    species=`echo $acc | awk '{match($0, /^_?[^_]+_[^_.]+/); print substr($0, RSTART, RLENGTH)}'` # Remove suffix
+    echo $acc,$total_IDP,$species
+done >> ./lib/IDP_percentage.csv
