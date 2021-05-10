6933a365da5ac29fa623b69c63542326e50a8100
Cheng-Hung Tsai <ctsai085@ucr.edu>
2021-05-10 14:06:38 -0700

    Add argparse options for phylo group, adjust x and y labels

--- scatterplot_genome_IDP_perc.py
+++ scatterplot_genome_IDP_perc.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
-import numpy as np
+import argparse
+import os
+import pathlib
 import pandas as pd
 import matplotlib.pyplot as plt
-# import seaborn as sns
 import plotly.express as px
 
+from util import fill_phylo_ranks
 
-# Merge metadata and IDP table
-meta_df = pd.read_csv('./lib/ensembl_metadata.csv', keep_default_na=False, na_values=None)
-IDP_df = pd.read_csv('./lib/IDP_percentage.csv')
-
-merged_df = pd.merge(meta_df, IDP_df, on='FILENAME')
-merged_df.to_csv('./lib/meta_IDP_perc.csv', index=False)
+# Parser
+avaliable_ranks = ['phylum', 'subphylum', 'class',
+                   'subclass', 'order', 'family', 'genus', 'species']
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--i', metavar='<Input dir>',
+                    help='Input directory (filter_{number})', required=True)
+parser.add_argument('--group', '-g', metavar='<phylo group>', choices=avaliable_ranks, default='subphylum',
+                    help=f'Used for catogorized y axis {avaliable_ranks} (default=\'subphylum\')')
+args = parser.parse_args()
+phylo_rank = args.group
 
 # Format dataframe
-merged_df = pd.read_csv('./lib/meta_IDP_perc.csv', keep_default_na=False, na_values=None)
-merged_df['IDP_perc'] = merged_df['TOTAL_IDP'] / merged_df['TOTAL_PROTEIN']
-phylo_df = merged_df[['NAME', 'ASM_LENGTH', 'IDP_perc', 'phylum', 'subphylum', 'class', 'subclass', 'order', 'family', 'genus']]
+df = pd.read_csv(os.path.join(args.i, 'IDP_perc_meta.csv'),
+                 keep_default_na=False, na_values=None)
+df['IDP_ratio_species'] = df['TOTAL_IDP']/df['TOTAL_PROTEIN']
 
-# Fill empty columns with higher level taxonomic ranks
-for col in phylo_df.iloc[:, 3:]:
-    na_idx = phylo_df[col] == ""
-    if na_idx.any() == True:
-        phylo_df.loc[na_idx, col] = phylo_df[previous_col][na_idx]
-    previous_col = col
+fill_phylo_ranks(df)
 
 # Scatterplot with plotly
-fig = px.scatter(phylo_df, x="ASM_LENGTH", y="IDP_perc", color="subphylum")
-fig.write_html("./figure/")
+fig = px.scatter(df, x='ASM_LENGTH',
+                 y='IDP_ratio_species', color=phylo_rank,
+                 labels={
+                     'ASM_LENGTH': 'Assembly length (bp)',
+                     'IDP_ratio_species': 'IDP ratio in species',
+                     f'{phylo_rank}': phylo_rank.capitalize()
+                 })
+
+# Check output path
+outpath = pathlib.Path(args.i, 'figure')
+outpath.mkdir(parents=True, exist_ok=True)
+fig.write_html(os.path.join(
+    outpath, f'scatterplot_IDP_ratio_groupby_{phylo_rank}.html'))
