b0d938f7937e18856d198a7b6ee4583902c6bb5f
Cheng-Hung Tsai <ctsai085@ucr.edu>
2021-05-10 14:01:28 -0700

    Modified output path

--- job_array.bash
+++ job_array.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -p batch
 #SBATCH -J iupred2a_in_fungi
-#SBATCH --time=7-00:00:00
+#SBATCH --time=3-00:00:00
 #SABTCH -n 8
 #SBATCH --mem=10g
 #SBATCH --array=0-1013%8
@@ -11,9 +11,7 @@ files=( pep/* )
 input=${files[$SLURM_ARRAY_TASK_ID]}
 sample=${input%.pep.all.fa.gz}
 sample=${sample#pep/}
-
-mkdir -p output
-output=output/$sample
+output="output/IDR"
 
 echo $SLURM_ARRAY_TASK_ID, $sample
-./run_iupred_genome-wide.py --i $input --o $output
+./2_run_iupred_genome-wide.py --i $input --o $output
